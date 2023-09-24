3e5a64e76bd4406c591ce9281882faa1cd8f5893
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-24 16:08:45 +0200

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Standard favicon -->
+        <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/favicons/favicon.ico') }}">
+
+        <!-- For iPhone and iPad -->
+        <link rel="apple-touch-icon" href="{{ Vite::asset('resources/images/favicons/apple-touch-icon.png') }}">
+
+        <!-- For Android -->
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ Vite::asset('resources/images/favicons/android-chrome-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="512x512" href="{{ Vite::asset('resources/images/favicons/android-chrome-512x512.png') }}">
+
+        <!-- For smaller resolutions -->
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/images/favicons/favicon-16x16.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/images/favicons/favicon-32x32.png') }}">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap" rel="stylesheet">
